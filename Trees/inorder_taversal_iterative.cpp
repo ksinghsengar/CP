3dfab076108aa0f11bbc87deb69cdeed3fec34a7
Kamini Singh <shkamini@thoughtworks.com>
2020-08-07 00:01:05 +0530

    Inorder traversal

---
+++ Trees/inorder_taversal_iterative.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> v;
+        stack<TreeNode*> s;
+        TreeNode * current = root;
+        while(current != NULL || !s.empty()){
+            while(current != NULL){
+                s.push(current);
+                current = current->left;
+            }
+            current = s.top();
+            v.push_back(current->val);
+            s.pop();
+            current = current->right;
+        }
+        
+    
+       return v; 
+    }
+};
